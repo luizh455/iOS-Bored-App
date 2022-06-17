d8d268bec9ee890218f781a5354652c854107a90
Luiz Lage <luiz.lage@mercadolivre.com>
2022-06-17 15:55:41 -0300

    first commit

---
+++ notBoredApp/notBoredAppUITests/notBoredAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  notBoredAppUITestsLaunchTests.swift
+//  notBoredAppUITests
+//
+//  Created by Luiz Henrique Lage Da Silva on 17/06/22.
+//
+
+import XCTest
+
+class notBoredAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
